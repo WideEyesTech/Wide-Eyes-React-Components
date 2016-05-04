2d92b793f0cdb816799e9424ab41ec845324e679
Gerard <gerard@wide-eyes.it>
2016-05-04 13:07:54 +0200

    1307

--- lib/hero.jsx
+++ lib/hero.jsx
@@ -3,11 +3,16 @@ import React from 'react'
 
 export const Hero = ({title, children}) =>
   <section className={hero.root}>
-    <header className={hero.header}>
-      <hgroup>
-        <h3 className="no-margin">{title}</h3>
-      </hgroup>
-    </header>
+    {title
+      ? (
+        <header className={hero.header}>
+          <hgroup>
+            <h3 className="no-margin">{title}</h3>
+          </hgroup>
+        </header>
+      )
+      : null
+    }
     <section className={hero.body}>
       {children}
     </section>
